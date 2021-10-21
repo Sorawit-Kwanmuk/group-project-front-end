cb7422991fe01573f8116108485ed35f3afdd246
wsuwit <w_suwit-456@hotmail.com>
2021-10-21 17:04:11 +0700

    Save CourseIfo update AddBtn

--- src/components/CourseAdmin/CourseAdmin.js
+++ src/components/CourseAdmin/CourseAdmin.js
@@ -5,148 +5,147 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import AddIcon from "@mui/icons-material/Add";
 
 function CourseAdmin() {
   return (
     <>
       <CssBaseline />
       <Container maxWidth="xlg">
-        <Card sx={{ minWidth: 275, bgcolor: "" }}>
+        <Card sx={{ minWidth: 275, bgcolor: "", marginY: 5 }}>
           <CardContent>
-            <CardContent>
-              <Typography gutterBottom variant="h5" align="center">
-                Course Information Form
-              </Typography>
-              <form>
-                <Grid container spacing={1}>
-                  <Grid xs={12} sm={6} item>
-                    <Grid container spacing={1}>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Course name"
-                          placeholder="Enter Course name"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Category"
-                          placeholder="Enter Category"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Level"
-                          placeholder="Enter Level"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Course duration"
-                          placeholder="Enter Course duration"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Price"
-                          placeholder="Enter Price"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Discount"
-                          placeholder="Enter Discount"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Discout until"
-                          placeholder="Enter Discout until"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Course image link"
-                          placeholder="Enter Course image link"
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
+            <Typography gutterBottom variant="h5" align="center">
+              Course Information Form
+            </Typography>
+            <form>
+              <Grid container spacing={1}>
+                <Grid xs={12} sm={6} item>
+                  <Grid container spacing={1}>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Course name"
+                        placeholder="Enter Course name"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
                     </Grid>
-                  </Grid>
-
-                  <Grid xs={12} sm={6} item>
-                    <Grid container spacing={1}>
-                      <Grid xs={12} item>
-                        <TextField
-                          label="Short description"
-                          placeholder="Enter Short description"
-                          multiline
-                          rows={5}
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-
-                      <Grid xs={12} item>
-                        <TextField
-                          label="About this course"
-                          placeholder="Enter About this course"
-                          multiline
-                          rows={8}
-                          variant="outlined"
-                          size="small"
-                          fullWidth
-                          required
-                        />
-                      </Grid>
-                      <Grid xs={12} item>
-                        <Button
-                          type="submit"
-                          variant="contained"
-                          color="primary"
-                          size="large"
-                          fullWidth
-                        >
-                          {"View & Edit"}
-                        </Button>
-                      </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Category"
+                        placeholder="Enter Category"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Level"
+                        placeholder="Enter Level"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Course duration"
+                        placeholder="Enter Course duration"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Price"
+                        placeholder="Enter Price"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Discount"
+                        placeholder="Enter Discount"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Discout until"
+                        placeholder="Enter Discout until"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Course image link"
+                        placeholder="Enter Course image link"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
                     </Grid>
                   </Grid>
                 </Grid>
-              </form>
-            </CardContent>
+
+                <Grid xs={12} sm={6} item>
+                  <Grid container spacing={1}>
+                    <Grid xs={12} item>
+                      <TextField
+                        label="Short description"
+                        placeholder="Enter Short description"
+                        multiline
+                        rows={5}
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+
+                    <Grid xs={12} item>
+                      <TextField
+                        label="About this course"
+                        placeholder="Enter About this course"
+                        multiline
+                        rows={8}
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid xs={12} item>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        fullWidth
+                      >
+                        {"View & Edit"}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </form>
           </CardContent>
           <CardActions
             style={{ display: "flex", justifyContent: "space-evenly" }}
@@ -172,6 +171,21 @@ function CourseAdmin() {
               </Button>
             </Grid>
           </CardActions>
+        </Card>
+        <Card
+          sx={{
+            minWidth: 275,
+            minHeight: 350,
+            bgcolor: "",
+            marginY: 5,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Button>
+            <AddIcon fontSize="large" />
+          </Button>
         </Card>
       </Container>
     </>
