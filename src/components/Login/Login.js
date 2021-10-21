b5144359ae365109786dbbee937713d6c6465d81
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-10-21 14:12:38 +0700

    Login สามารถทำงานได้ เหลือ validate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,10 +10,12 @@ import { useState } from 'react';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import './styleLogin.css';
+import axios from '../../config/axios';
+import { setToken, getToken } from '../../services/localStorage';
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [values, setValues] = useState({
-    username: '',
-    password: '',
     showPassword: false,
   });
   console.log(values);
@@ -26,57 +28,73 @@ function Login() {
   const handleMouseDownPassword = event => {
     event.preventDefault();
   };
-  const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.value });
+  const handleSubmitLogin = async e => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post('/auth/login', {
+        username,
+        password,
+      });
+      console.log(res);
+      // setToken(res.data.token);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <>
-      <div className='LoginForm'>
-        <span className='spanLogin'>Login</span>
-        <div className='divLogin'>
-          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-            <TextField
-              id='outlined-basic'
-              type='text'
-              label='Username'
-              variant='outlined'
-              value={values.username}
-              onChange={handleChange('username')}
-            />
-          </Box>
+      <form action='' onSubmit={handleSubmitLogin}>
+        <div className='LoginForm'>
+          <span className='spanLogin'>Login</span>
+          <div className='divLogin'>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <TextField
+                id='outlined-basic'
+                type='text'
+                label='Username'
+                variant='outlined'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </Box>
+          </div>
+          <div className='divLogin'>
+            <FormControl sx={{ m: 1, width: '26.25ch' }} variant='outlined'>
+              <InputLabel htmlFor='outlined-adornment-password'>
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id='outlined-adornment-password'
+                type={values.showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                endAdornment={
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge='end'>
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label='Password'
+              />
+            </FormControl>
+          </div>
+          <div className='divLogin buttonLogin'>
+            <Button variant='contained' type='submit'>
+              Login
+            </Button>
+          </div>
+          <div className='divLogin'>
+            <Button variant='contained'>Register</Button>
+          </div>
         </div>
-        <div className='divLogin'>
-          <FormControl sx={{ m: 1, width: '26.25ch' }} variant='outlined'>
-            <InputLabel htmlFor='outlined-adornment-password'>
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id='outlined-adornment-password'
-              type={values.showPassword ? 'text' : 'password'}
-              value={values.password}
-              onChange={handleChange('password')}
-              endAdornment={
-                <InputAdornment position='end'>
-                  <IconButton
-                    aria-label='toggle password visibility'
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge='end'>
-                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label='Password'
-            />
-          </FormControl>
-        </div>
-        <div className='divLogin buttonLogin'>
-          <Button variant='contained'>Login</Button>
-        </div>
-        <div className='divLogin'>
-          <Button variant='contained'>Register</Button>
-        </div>
-      </div>
+      </form>
       <div className='divCloseAll'></div>
     </>
   );
