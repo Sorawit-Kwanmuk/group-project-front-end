27b6d106ad7fd8eb9447ce7fce1cac33eada4b52
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-10-21 12:16:19 +0700

    error

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -3,9 +3,24 @@ import Avatar from '@mui/material/Avatar';
 import John from '../../public/images/john.jpg';
 import { imageConfig } from './muiConfig';
 import { useState } from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import { TextField } from '@mui/material';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import MuiPickersUtilsProvider from '@material-ui/pickers';
 
 function ShoppingCart() {
   const [image, setImage] = useState({ profileImage: '' });
+  const [payment, setPayment] = useState('Visa');
+  const [value, setValue] = useState(new Date());
+
+  const handleChange = event => {
+    setPayment(event.target.value);
+  };
 
   return (
     <>
@@ -33,7 +48,70 @@ function ShoppingCart() {
             <div className='grayLine'></div>
             <div className='MockUpCourseCard'></div>
           </div>
-          <div className='inputPayment'></div>
+          <div className='inputPayment'>
+            <div className='inputPaymentCalculate'>
+              <h2>Total:</h2>
+              <div className='inputPaymentCalculateAmount'>
+                <h1>3000</h1>
+                <h1>THB</h1>
+              </div>
+              <div className='grayLine'></div>
+            </div>
+            <div className='divFormAddPayment'>
+              <form>
+                <div className='divPaymentRadio'>
+                  <RadioGroup
+                    name='controlled-radio-buttons-group'
+                    value={payment}
+                    onChange={handleChange}>
+                    <FormControlLabel
+                      value='visa'
+                      control={<Radio />}
+                      label='Visa'
+                    />
+                    <FormControlLabel
+                      value='masterCard'
+                      control={<Radio />}
+                      label='MasterCard'
+                    />
+                  </RadioGroup>
+                </div>
+                <div className='textFieldColumPayment'>
+                  <TextField
+                    id='outlined-basic'
+                    label='Cardholder Name'
+                    variant='outlined'
+                  />
+                  <TextField
+                    id='outlined-basic'
+                    label='Card Number'
+                    variant='outlined'
+                  />
+                </div>
+
+                <div>
+                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <DatePicker
+                      views={['year']}
+                      label='Year only'
+                      value={value}
+                      onChange={newValue => {
+                        setValue(newValue);
+                      }}
+                      renderInput={params => (
+                        <TextField {...params} helperText={null} />
+                      )}
+                    />
+                  </MuiPickersUtilsProvider>
+                  <TextField
+                    id='outlined-basic'
+                    label='CCV'
+                    variant='outlined'
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
