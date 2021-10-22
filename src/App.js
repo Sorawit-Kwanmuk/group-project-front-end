dc78dcdbdfc0b123cf939e3ed68656c059928759
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-10-22 18:16:00 +0700

    add css not success

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import "./App.css";
-import CourseCard from "./components/CourseCard/CourseCard";
-import CourseClassroomAdmin from "./components/CourseAdmin/CourseClassroomAdmin";
-import MainCourseAdmin from "./components/CourseAdmin/MainCourseAdmin";
-import ContactUsUser from "./components/ContactUs/ContactUsUser";
-import ContactUsEdit from "./components/ContactUs/ContactUsEdit";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import MyProfile from "./components/MyProfile/MyProfile";
-import Register from "./components/Register/Register";
-import ForgetPassword from "./components/ForgetPassword/ForgetPassword";
-import Login from "./components/Login/Login";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
-import OurCourse from "./components/OurCourse/OurCourse";
-import OurTeam from "./components/OurTeam/OurTeam";
-import Instructor from "./components/InstructorCard/InstructorCard";
-import "./App.css";
-import ClassroomILearn from "./components/ClassroomILearn/ClassroomILearn";
+import './App.css';
+import CourseCard from './components/CourseCard/CourseCard';
+import CourseClassroomAdmin from './components/CourseAdmin/CourseClassroomAdmin';
+import MainCourseAdmin from './components/CourseAdmin/MainCourseAdmin';
+import ContactUsUser from './components/ContactUs/ContactUsUser';
+import ContactUsEdit from './components/ContactUs/ContactUsEdit';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import MyProfile from './components/MyProfile/MyProfile';
+import Register from './components/Register/Register';
+import ForgetPassword from './components/ForgetPassword/ForgetPassword';
+import Login from './components/Login/Login';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+import OurCourse from './components/OurCourse/OurCourse';
+import OurTeam from './components/OurTeam/OurTeam';
+import Instructor from './components/InstructorCard/InstructorCard';
+import './App.css';
+import ClassroomILearn from './components/ClassroomILearn/ClassroomILearn';
+import ShoppingCard from './components/ShoppingCard/ShoppingCard';
 
 function App() {
   return (
     <>
       <Header />
-      <CourseCard />
+      {/* <CourseCard />
       <MyProfile />
       <ForgetPassword />
       <Register />
@@ -34,7 +35,8 @@ function App() {
       <CourseClassroomAdmin />
       <ContactUsEdit />
       <ContactUsUser />
-      <ClassroomILearn />
+      <ClassroomILearn /> */}
+      <ShoppingCard />
       <Footer />
     </>
   );
---
+++ src/components/ShoppingCard/ShoppingCard.js
@@ -0,0 +1,13 @@
+import './styleShoppingCard.css';
+import shoppingCardBanner from '../../public/images/shoppingCard.png';
+function ShoppingCard() {
+  return (
+    <div>
+      <div>
+        <img src='' alt='' />
+      </div>
+    </div>
+  );
+}
+
+export default ShoppingCard;
