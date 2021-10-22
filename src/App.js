d3d7ab3fb5e387f484fbe863274f815d7d44019a
wsuwit <w_suwit-456@hotmail.com>
2021-10-22 15:51:03 +0700

    Try commit error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import CourseCard from "./components/CourseCard/CourseCard";
-import ContactUsEdit from "./components/ContactUs/ContactUsEdit";
 import CourseClassroomAdmin from "./components/CourseAdmin/CourseClassroomAdmin";
 import MainCourseAdmin from "./components/CourseAdmin/MainCourseAdmin";
 import Footer from "./components/Footer/Footer";
@@ -13,26 +12,22 @@ import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import OurCourse from "./components/OurCourse/OurCourse";
 import OurTeam from "./components/OurTeam/OurTeam";
 import Instructor from "./components/InstructorCard/InstructorCard";
-import "./App.css";
-import CourseAdmin from "./components/CourseAdmin/CourseAdmin";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <CourseCard /> */}
-      {/* <ContactUsEdit /> */}
-      {/* <MyProfile /> */}
-      {/* <ForgetPassword /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <ShoppingCart /> */}
-      {/* <OurCourse /> */}
-      {/* <OurTeam /> */}
-      {/* <Instructor /> */}
-      {/* <CourseAdmin /> */}
-      {/* <MainCourseAdmin /> */}
-      {/* <CourseClassroomAdmin /> */}
+      <CourseCard />
+      <MyProfile />
+      <ForgetPassword />
+      <Register />
+      <Login />
+      <ShoppingCart />
+      <OurCourse />
+      <OurTeam />
+      <Instructor />
+      <MainCourseAdmin />
+      <CourseClassroomAdmin />
       <Footer />
     </>
   );
