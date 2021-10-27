0a27387a8b9dcb665d7e4d7dbebe1dd8331b2fab
wsuwit <w_suwit-456@hotmail.com>
2021-10-27 19:49:12 +0700

    Save before checkout -b user-class

--- src/components/ClassroomILearn/ContentRightClass/ContentRightClass.js
+++ src/components/ClassroomILearn/ContentRightClass/ContentRightClass.js
@@ -1,41 +1,41 @@
-import '../styleClassroomILearn.css';
-import Button from '@mui/material/Button';
-import { ButtonConfig, ButtonConfig2 } from '../muiConfig';
-import VideoLink from './VideoLink/VideoLink';
-import QuizTest from './Quiz/QuizTest';
+import "../styleClassroomILearn.css";
+import Button from "@mui/material/Button";
+import { ButtonConfig, ButtonConfig2 } from "../muiConfig";
+import VideoLink from "./VideoLink/VideoLink";
+import QuizTest from "./Quiz/QuizTest";
 function ContentRightClass() {
   return (
-    <div className='mainDivControllerContentRightClass'>
-      <div className='navBarContentRightClass'>
-        <Button variant='contained' sx={ButtonConfig}>
+    <div className="mainDivControllerContentRightClass">
+      <div className="navBarContentRightClass">
+        <Button variant="contained" sx={ButtonConfig}>
           My Course
         </Button>
-        <div className='textLabelContentRightClass'>HTML - Element / Tag</div>
-        <Button variant='contained' sx={ButtonConfig}>
+        <div className="textLabelContentRightClass">HTML - Element / Tag</div>
+        <Button variant="contained" sx={ButtonConfig}>
           Completed !
         </Button>
       </div>
-      <div className='lineCompleteController'>
-        <div className='lineCompleteStatusSuccess'></div>
-        <div className='lineWhiteCompleteStatusSuccess'></div>
-        <div className='lineCompleteStatus'></div>
-        <div className='divLabelPercentComplete'>
+      <div className="lineCompleteController">
+        <div className="lineCompleteStatusSuccess"></div>
+        <div className="lineWhiteCompleteStatusSuccess"></div>
+        <div className="lineCompleteStatus"></div>
+        <div className="divLabelPercentComplete">
           <p>80</p>
           <p>%</p>
           <p>Complete</p>
         </div>
       </div>
-      {/* <VideoLink /> */}
+      <VideoLink />
       <QuizTest />
       <QuizTest />
       <QuizTest />
       <QuizTest />
       <QuizTest />
-      <div className='divControlButtonBottom'>
-        <Button sx={ButtonConfig2} variant='contained'>
+      <div className="divControlButtonBottom">
+        <Button sx={ButtonConfig2} variant="contained">
           Previous
         </Button>
-        <Button sx={ButtonConfig2} variant='contained'>
+        <Button sx={ButtonConfig2} variant="contained">
           Next
         </Button>
       </div>
