45d6169000dc33da9937fa7748948c2d79e7607f
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-11-02 14:20:15 +0700

    calousel success

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { PrevButton, NextButton } from './CarouselButton';
 import useEmblaCarousel from 'embla-carousel-react';
-import { bannerByIndex } from './media/index';
 import './embla.css';
 import { CarouselContext } from '../../contexts/CarouselContext';
+import { useHistory } from 'react-router-dom';
 function Carousel({ slides }) {
   const [viewportRef, embla] = useEmblaCarousel({
     loop: true,
     skipSnaps: false,
   });
-  // const carouselContext = useContext(CarouselContext);
+  const history = useHistory();
+  const carouselContext = useContext(CarouselContext);
+  const { carouselImage, bannerImage, bannerLinkTo, bannerByIndex } =
+    carouselContext;
 
   const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
   const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
   const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
   const scrollNext = useCallback(() => embla && embla.scrollNext(), [embla]);
+
   const onSelect = useCallback(() => {
     if (!embla) return;
     setPrevBtnEnabled(embla.canScrollPrev());
@@ -26,6 +30,9 @@ function Carousel({ slides }) {
     embla.on('select', onSelect);
     onSelect();
   }, [embla, onSelect]);
+  const handleClickLinkToPage = index => {
+    history.push(bannerLinkTo[index]);
+  };
   return (
     <>
       <div className='embla'>
@@ -36,8 +43,9 @@ function Carousel({ slides }) {
                 <div className='embla__slide__inner'>
                   <img
                     className='embla__slide__img'
+                    onClick={() => handleClickLinkToPage(index)}
                     src={bannerByIndex(index)}
-                    alt='A cool cat.'
+                    alt='banner'
                   />
                 </div>
               </div>
--- src/components/Carousel/CarouselIndex.js
+++ src/components/Carousel/CarouselIndex.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CarouselContext } from '../../contexts/CarouselContext';
 import Carousel from './Carousel';
 
 function CarouselIndex() {
-  const SLIDE_COUNT = 5;
+  const { carouselImage } = useContext(CarouselContext);
+  const SLIDE_COUNT = carouselImage.length;
   const slides = Array.from(Array(SLIDE_COUNT).keys());
+  // console.log('slides: ', slides);
   return (
     <div>
       <Carousel slides={slides} />
